11fc9cf04adbc8647ba0139f4fd3b3a574d78208
Viacheslav Ivanov <tristandtm@gmail.com>
2021-11-05 18:10:25 +0000

    Add autoscaling and clusterization

---
+++ autoscaling.tf
@@ -0,0 +1,11 @@
+resource "aws_autoscaling_group" "example" {
+	launch_configuration = aws_launch_configuration.example.name
+	vpc_zone_identifier = data.aws_subnet_ids.default.ids
+	min_size = 2
+	max_size = 10
+	tag {
+		key = "Name"
+		value = "terraform-asg-example"
+		propagate_at_launch = true
+	}
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "example" {
-	ami = "ami-08edbb0e85d6a0a07"
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-08edbb0e85d6a0a07"
 	instance_type = "t2.micro"
-	vpc_security_group_ids = [aws_security_group.instance.id]
+	security_groups = [aws_security_group.instance.id]
 	user_data =  <<-EOF
 		#!/bin/bash
 		echo "Hello, World" > index.html
 		nohup busybox httpd -f -p ${var.server_port} &
 		EOF
-	tags = {
-		Name = "terraform-example"
+	lifecycle {
+		create_before_destroy = true
 	}
 }
